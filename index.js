1b64e4590e613000314f315099d049283a40ef05
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-01-30 22:25:06 +0530

    Add Nav-bar to the mobile version

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Javascript was not used in this project yet
+const menu = document.getElementById('mobile-menu')
+const menubtn = document.getElementById('menu-bar')
+const closebar = document.getElementsByClassName('close-btn')[0]
+const main = document.querySelector('.bg')
+menubtn.onclick = function display () {
+  menu.style.display = 'block'
+  main.style.filter = 'blur(8px)'
+  menubtn.style.display = 'none'
+}
+
+closebar.onclick = function close () {
+  menu.style.display = 'none'
+  menubtn.style.display = 'block'
+}
+
+window.onclick = function exit (event) {
+  if (event.target === menu) {
+    menu.style.display = 'none'
+  }
+}
+
+menu.onclick = function close () {
+  menu.style.display = 'none'
+}
